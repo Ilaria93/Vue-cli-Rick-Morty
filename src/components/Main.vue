3a2495fac06e711c1731dacce2da3f9f9dafb6e9
Ilaria Pascucci <kiraleto@gmail.com>
2021-09-12 13:27:14 +0200

    creazione progetto rickandmorty

---
+++ src/components/Main.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="row">
+        <Character class = "col-6 col-md-3" v-for = "character in characters" :key = "character" 
+        :name = 'character.name' 
+        :origin = 'character.origin'
+        :species = 'character.species'
+        :image = 'character.image'
+        />
+    </div>
+</template>
+
+<script>
+import Character from './Character.vue'
+export default {
+    name:'Main',
+    components: {
+        Character
+    },
+    props: {
+        characters: Array
+    }
+        
+}
+</script>
+
+<style lang = "scss" scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+//libreria di boostrap js //
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+//VUE//
 
 Vue.config.productionTip = false
 
